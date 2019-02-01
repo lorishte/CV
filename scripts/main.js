e91e444ac727ab59050b15a4e9c2906f685761a3
Lora Milcheva <lora.milcheva@outlook.com>
2019-02-01 14:35:58 +0200

    Small changes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,31 @@
 $(document).ready(function () {
-	console.log(222)
-});
 
-// Get all links
-// Remove links that don't actually link to anything
-$('a[href*="#"]')
-	.not('[href="#"]')
-	.not('[href="#0"]')
-	.not('[href=""]')
-	.on('click', function (e) {
-		e.preventDefault();
-		$(document).off('scroll');
+	let menuLinks = $('.nav-link');
+	let headerHeight = $('header').height();
 
-		let target = $(this).attr('href'); //Get the target
-		let scrollToPosition = $(target).offset().top
-
-		//smooth scroll
-		$('html, body').animate({
-			'scrollTop': scrollToPosition
-		}, 1000, 'swing', function () {
-			window.location.hash = target;
-			$('html').animate({'scrollTop': scrollToPosition + 1}, 0);
-		});
+	// Make menu active
+	menuLinks.click(function () {
+		menuLinks.removeClass('active');
+		$(this).addClass('active');
 	});
+
+	//smooth scroll
+	$('a[href*="#"]')
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.not('[href=""]')
+		.on('click', function (e) {
+			e.preventDefault();
+			$(document).off('scroll');
+
+			let target = $(this).attr('href'); //Get the target
+			let scrollToPosition = $(target).offset().top - headerHeight;
+
+			$('html, body').animate({
+				'scrollTop': scrollToPosition
+			}, 1000, 'swing', function () {
+				window.location.hash = target;
+				$('html').animate({'scrollTop': scrollToPosition + 1}, 0);
+			});
+		});
+});
